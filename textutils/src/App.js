9d47e428a319ce1fa9e77aa455e7e4781215eb0c
Anurag Kumar <anuragkumarrr@gmail.com>
2022-11-13 21:44:17 -0600

    Added Alert component

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,12 +1,47 @@
+import { useState } from 'react';
+// import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+
 function App() {
+  const [mode,setMode] = useState('light');
+  const [alert,setAlert] = useState(null)
+
+  const showAlert =(message,type) =>{
+    setAlert({
+      msg: message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () =>{
+  //  var val = (mode==='light')?'dark':'light'
+   if(mode==='light')
+    {
+      setMode('dark')
+      document.body.style.backgroundColor='#042743'
+      showAlert('Dark Mode has been enabled','success')
+    
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      
+      showAlert('Light Mode has been enabled','success')
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container">
-      <TextForm heading="Enter the text to analyse below"/>
+      <TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyse below"/>
+      {/* <About/> */}
       </div>
      </>
   );
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -7,22 +7,31 @@ export default function TextForm(props) {
         setText((a)=>{
             return a.toUpperCase()
         })
+        props.showAlert('Converted to Uppercase','success')
     }
     const handleLoCLick = ()=>{
         // console.log('LowerCase was clicked')
            setText((a)=>{
             return a.toLowerCase()
         })
+        props.showAlert('Converted to Lowercase','success')
     }
     const handleReverseCLick = ()=>{
         // console.log('Reverse was clicked')
            setText((a)=>{
             return a.split('').reverse().join('')
         })
+        props.showAlert('The text has been reversed','success')
     }
     const handleClearCLick = ()=>{
         // console.log('Clear was clicked')
            setText("")
+           props.showAlert('The text has been cleared','success')
+    }
+    const handleSpaceCLick = ()=>{
+        // console.log('Clear was clicked')
+           setText(text. split(''). join(' ') )
+           props.showAlert('The space has been applied','success')
     }
     const handleOnChange = (event)=>{
         // console.log('On Change')
@@ -32,28 +41,29 @@ export default function TextForm(props) {
   // Declare a new state variable, which we'll call "text"
   const [text, setText] = useState('');
 return (
-<div>
+
+
     <>
-    <div className="container my-4">
+    <div className="container my-4" style={{color:props.mode==='dark'?'white':'#042743'}}>
     <h2>{props.heading}</h2>
     <div className="mb-3" >
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleOnChange}></textarea>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743'}}></textarea>
     </div>  
     <div>
       <button className="btn btn-primary mx-2" onClick={handleUpCLick}>Convert to Uppercase</button>
       <button className="btn btn-primary mx-2" onClick={handleLoCLick}>Convert to Lowercase</button>
     <button className="btn btn-primary mx-2" onClick={handleReverseCLick}>Reverse All</button>
     <button className="btn btn-primary mx-2" onClick={handleClearCLick}>Clear Text</button>
+    <button className="btn btn-primary mx-2" onClick={handleSpaceCLick}>Space up Text</button>
     </div>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
         <h3>Your Text Summary</h3>
         <p>{text.split(' ').length} Words, {text.length} Characters</p>
         <p>{0.008*text.split(' ').length} minutes to read.</p>
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length>0? text:'Enter the text for preview :)'}</p>
     </div>
     </>
-</div>
 )
 }
