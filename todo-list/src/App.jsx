b9d9e41740b41dce0fa90df571e4ab3f14894f3e
Anurag Kumar <c-anurag.kumar@charter.com>
2023-09-26 09:35:34 -0500

    Added Delete funtionality

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from './components/navbar'
 import Footer from './components/Footer'
 import Todos from './components/Todos'
 
 function App() {
-  const onDelete =(todo)=>{
-    console.log('Deleted',todo)
-  }
-  let todos = [
+  const [todos,setTodos] = useState([
     {
       sno:1,
       title:"Do workout",
@@ -23,7 +20,15 @@ function App() {
       title:"Do Work",
       desc:"Go need to office"
     }
-  ]
+  ])
+  const onDelete =(todo)=>{
+    console.log('Delete',todo)
+    setTodos(todos.filter((e)=>{
+      console.log("Deleted",e)
+      return e!==todo;
+    }))
+  }
+
   return (
     <div>
       <Navbar title="Todo-List" searchBar={false}/>
--- todo-list/src/components/Todos.jsx
+++ todo-list/src/components/Todos.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { TodoItem } from './TodoItem'
 
- const Todos = (props) =>{
+const Todos = (props) => {
   return (
     <div className='container'>
       <h3 className='text-center my-3'>Todos List</h3>
-      {props.todos.map((todo)=>{
-        return ( <TodoItem key={todo.sno} todo={todo} onDelete={props.onDelete}/>)
-      })}
-       
+      {props.todos.length === 0 ? "No Todos to display" :
+        props.todos.map((todo) => {
+          return (<TodoItem key={todo.sno} todo={todo} onDelete={props.onDelete} />)
+        })
+      }
+
     </div>
   )
 }
