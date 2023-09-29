1454764ec10d6d66868d934e13f8a1de17685db6
Anurag Kumar <c-anurag.kumar@charter.com>
2023-09-29 14:29:07 -0500

    Updated the local storage code

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,53 +1,49 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from './components/navbar'
 import Footer from './components/Footer'
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
 
 function App() {
-  const [todos,setTodos] = useState([
-    {
-      sno:1,
-      title:"Do workout",
-      desc:"Go need to gym"
-    },
-    {
-      sno:2,
-      title:"Do Shopping",
-      desc:"Go need to shop"
-    },
-    {
-      sno:3,
-      title:"Do Work",
-      desc:"Go need to office"
-    }
-  ])
-  const onDelete =(todo)=>{
-    // console.log('Delete',todo)
-    setTodos(todos.filter((e)=>{
-      // console.log("Deleted",e)
-      return e!==todo;
-    }))
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem('todos'));
   }
-  const addTodo =(title,desc)=>{
-      // console.log("Adding the details",title,desc);
-      // let sno =  todos[todos.length-1].sno+1
-      let sno =  todos.length==0 ? 1 : todos[todos.length-1].sno + 1
-    
-      const myTodo ={
-        sno:sno,
-        title:title,
-        desc:desc
-      }
-      setTodos([...todos,myTodo])
+  const [todos, setTodos] = useState(initTodo)
+  
+  const onDelete = (todo) => {
+    // console.log('Delete',todo)
+    setTodos(todos.filter((e) => {
+      // console.log("Deleted",e)
+      return e !== todo;
+    }))
+    localStorage.setItem("todos", JSON.stringify(todos))
   }
 
+  const addTodo = (title, desc) => {
+    // console.log("Adding the details",title,desc);
+    // let sno =  todos[todos.length-1].sno+1
+    let sno = todos.length == 0 ? 1 : todos[todos.length - 1].sno + 1
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myTodo])
+  }
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
   return (
     <div>
-      <Navbar title="Todo-List" searchBar={false}/>
-      <AddTodo addTodo={addTodo}/>
-      <Todos todos={todos} onDelete={onDelete}/>
-      <Footer/>
+      <Navbar title="Todo-List" searchBar={false} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
     </div>
   )
 }
