6aa9b278fb33c984ab5ffbbbb36ef99a1a33820f
Anurag Kumar <c-anurag.kumar@charter.com>
2024-06-19 12:32:50 -0500

    Updated the css fix

--- create-first-comp-KG/src/VegetableFragment/Fragment.jsx
+++ create-first-comp-KG/src/VegetableFragment/Fragment.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
 function Fragment() {
+    const foodItems = [
+        'Lentilis', 'Vegetable', 'Fruits', 'Milk', 'Rottis'
+    ]
     return (
         <div>
             <h3> Healthy Food List</h3>
-
             <ul class="list-group">
+                {Object.values(foodItems).map((value) => (
+                    <li class="list-group-item">{value}</li>
+                ))}
+
+            </ul>
+            {/* <ul class="list-group">
                 <li class="list-group-item">Lentilis</li>
                 <li class="list-group-item">Vegetable</li>
                 <li class="list-group-item">Fruits</li>
                 <li class="list-group-item">Milk</li>
                 <li class="list-group-item">Rottis</li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
--- create-first-comp-KG/src/todo-basic/TodoItem1.jsx
+++ create-first-comp-KG/src/todo-basic/TodoItem1.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function TodoItem1() {
+function TodoItem1(props) {
 
     let todoName = "Buy Milk"
     let todoDate = "4/10/2024"
+    let todoItems = [
+        {
+            todoName: 'Buy Milk',
+            todoDate: "4/10/2024"
+        },
+        {
+            todoName: 'Go to Gym',
+            todoDate: "4/11/2024"
+        }
+    ]
     return (
         <div>
-            <div className="container">
+            {
+                todoItems.map((data) => (
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
+                                {data.todoName}
+                            </div>
+                            <div className="col-4">
+                                {data.todoDate}
+                            </div>
+                            <div className="col-2">
+                                <button type="button" className="btn btn-danger kg-button">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+                )
+            }
+
+            {/* <div className="container">
                 <div className="row">
                     <div className="col-6">
                         {todoName}
@@ -18,7 +47,7 @@ function TodoItem1() {
                         <button type="button" className="btn btn-danger kg-button">Delete</button>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
         </div >
     )
